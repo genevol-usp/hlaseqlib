3eed7c8f020ded7a00c34903acdf30be5c10ab83
VitorAguiar <vitor.aguiar@me.com>
2017-07-25 09:30:59 -0300

    replace external functions with base

--- R/hla_readtx.R
+++ R/hla_readtx.R
@@ -8,26 +8,25 @@ hla_readtx <- function(x) {
   tx_names <- 
     c("A", "B", "C", "DQA1", "DQB1", "DRB1") %>%
     rep(each = 2) %>%
-    stringr::str_c(1:2, sep = "_") %>%
-    c("sample", ., stringr::str_c(., "_est_counts"))
+    paste(1:2, sep = "_") %>%
+    c("subject", ., paste0(., "_est_counts"))
 
   hla <-
     readr::read_csv(x, col_names = FALSE) %>%
     dplyr::select(-(X14:X25)) %>%
-    purrr::set_names(tx_names) %>%
-    dplyr::mutate(sample = stringr::str_replace(sample, "\\.0$", "")) %>%
+    setNames(tx_names) %>%
+    dplyr::mutate(subject = sub("\\.0$", "", subject)) %>%
     dplyr::distinct() %>%
-    dplyr::arrange(sample)
+    dplyr::arrange(subject)
 
-    hla_tidy <- 
-      tibble::tibble(subject = rep(hla$sample, each = 12), 
-		     allele = NA, est_counts = NA)
+    hla_tidy <- tibble::tibble(subject = rep(hla$subject, each = 12), 
+			       allele = NA, est_counts = NA)
 
     counter <- 1
     for (i in seq_len(nrow(hla))) {
       hla_tidy$allele[counter:(counter+11)] <- unlist(hla[i, 2:13])
       hla_tidy$est_counts[counter:(counter+11)] <- unlist(hla[i, 14:25])
-      counter %<>% `+`(12)
+      counter <- counter + 12
    }
 
    hla_tidy
