a2f4b9c1f3d8291c031542098be424032e91f3b1
VitorAguiar <vitor.aguiar@me.com>
2017-08-24 13:26:54 -0300

    experimental: redesign code, filter combinations which contains all alleles than assess the most likely given distance to ref

--- R/phase_hla.R
+++ R/phase_hla.R
@@ -12,80 +12,46 @@ phase_hla <- function(x) {
 
   x <- dplyr::mutate(x, locus = sub("^HLA-", "", locus))
 
-  het_loci <-
-    x %>%
-      dplyr::group_by(locus) %>%
-      dplyr::filter(dplyr::n_distinct(allele) == 2) %>%
-      dplyr::pull(locus) %>%
-      unique
-
   combinations <- 
     x %>%
       dplyr::group_by(locus, hap) %>%
       dplyr::mutate(i = seq_len(n())) %>%
       tidyr::unite(allele_diff, allele, diffs) %>%
       tidyr::spread(locus, allele_diff) %>%
-      tidyr::fill(subject:DRB1) %>%
       tidyr::expand(hap, A, B, C, DQA1, DQB1, DRB1) %>%
       purrr::by_row(. %>% dplyr::select(A:DRB1) %>% sub("^.+_(\\d+)$", "\\1", .) %>% as.integer %>% sum,
 		    .to = "S", .collate = "cols") %>%
       dplyr::mutate_at(dplyr::vars(A:DRB1), . %>% sub("_\\d+$", "", .))
 
-  min_hap <- 
-    combinations %>%
-    dplyr::filter(S == min(S)) %>%
-    dplyr::group_by(hap) %>%
-    dplyr::mutate(n = n()) %>%
-    dplyr::ungroup()
+  h1s <- dplyr::filter(combinations, hap == "a1") %>% dplyr::mutate(i = "A")
+  h2s <- dplyr::filter(combinations, hap == "a2") %>% dplyr::mutate(i = "A")
 
-  if (any(min_hap$n == 1L)) min_hap <- dplyr::filter(min_hap, n == 1L)
+ test.x <- dplyr::left_join(h1s, h2s, by = "i") %>% 
+   dplyr::select(-i) %>%
+   dplyr::mutate(r = 1:n())
+  
+ test.y <- test.x %>%
+   dplyr::select(-S.x, -S.y) %>%
+   tidyr::gather(locus, allele, -hap.x, -hap.y, -r) %>%
+   dplyr::arrange(r, hap.x, hap.y, locus) %>%
+   dplyr::group_by(r) %>%
+   dplyr::filter(all(x$allele %in% allele)) %>%
+   dplyr::ungroup()
 
-  min_hap <- dplyr::select(min_hap, -n)
+ test.z <- dplyr::filter(test.x, r %in% test.y$r) %>%
+   dplyr::select(-r) %>%
+   dplyr::mutate(S = S.x + S.y) %>%
+   dplyr::filter(S == min(S))
 
-  if (all(c("a1", "a2") %in% min_hap$hap) && 
-      all(x$allele %in% unlist(min_hap)) && 
-      !any(duplicated(min_hap$hap))) {
-     return(min_hap)
-  }
+ out.x <- dplyr::select(test.z, dplyr::ends_with(".x")) %>%
+   setNames(sub("\\.x", "", names(.)))
+ out.y <- dplyr::select(test.z, dplyr::ends_with(".y")) %>%
+   setNames(sub("\\.y", "", names(.)))
 
-  amb_loci <- 
-    min_hap %>%
-    tidyr::gather(locus, allele, -hap, -S) %>%
-    dplyr::group_by(hap, locus) %>%
-    dplyr::filter(dplyr::n_distinct(allele) > 1) %>%
-    dplyr::pull(locus) %>%
-    unique()
+ out <- dplyr::bind_rows(out.x, out.y) %>%
+   dplyr::arrange(hap)
 
-  min_alleles <- 
-    min_hap[het_loci] %>%
-    purrr::keep(~dplyr::n_distinct(.) == 1) %>% 
-    unlist
-
-  sec_hap <-
-    combinations %>%
-      dplyr::filter(hap != min_hap$hap,
-		    ! A %in% min_alleles, ! B %in% min_alleles, 
-		    ! C %in% min_alleles, ! DQA1 %in% min_alleles, 
-		    ! DQB1 %in% min_alleles, ! DRB1 %in% min_alleles) %>%
-      dplyr::filter(S == min(S))
-
-  amb_in_hap2 <-
-    sec_hap[amb_loci] %>%
-    purrr::keep(~dplyr::n_distinct(.) == 1) %>%
-    unlist
-
-  min_hap <- min_hap %>%
-    dplyr::filter(! A %in% amb_in_hap2, ! B %in% amb_in_hap2, 
-		  ! C %in% amb_in_hap2, ! DQA1 %in% amb_in_hap2, 
-		  ! DQB1 %in% amb_in_hap2, ! DRB1 %in% amb_in_hap2)
-
-  if(all(c("a1", "a2") %in% min_hap$hap) && 
-     !all(c("a1", "a2") %in% sec_hap$hap)) {
-
-    min_hap <- dplyr::filter(min_hap, ! hap %in% sec_hap$hap)
-  }
-
-  dplyr::bind_rows(min_hap, sec_hap) %>%
+ out %>%
     dplyr::group_by(hap) %>%
     dplyr::summarize_all(. %>% unique %>% paste(collapse = "/")) 
 }
