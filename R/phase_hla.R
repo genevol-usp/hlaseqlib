f79dde3715902c8e009b9fe7762b8e6c3c0188bc
VitorAguiar <vitor.aguiar@me.com>
2017-08-25 10:39:44 -0300

    avoid artefactual ambiguity when one of the haps is clearly the winner

--- R/phase_hla.R
+++ R/phase_hla.R
@@ -26,32 +26,58 @@ phase_hla <- function(x) {
   h1s <- dplyr::filter(combinations, hap == "a1") %>% dplyr::mutate(i = "A")
   h2s <- dplyr::filter(combinations, hap == "a2") %>% dplyr::mutate(i = "A")
 
- test.x <- dplyr::left_join(h1s, h2s, by = "i") %>% 
-   dplyr::select(-i) %>%
-   dplyr::mutate(r = 1:n())
-  
- test.y <- test.x %>%
-   dplyr::select(-S.x, -S.y) %>%
-   tidyr::gather(locus, allele, -hap.x, -hap.y, -r) %>%
-   dplyr::arrange(r, hap.x, hap.y, locus) %>%
-   dplyr::group_by(r) %>%
-   dplyr::filter(all(x$allele %in% allele)) %>%
-   dplyr::ungroup()
+  test.x <- dplyr::left_join(h1s, h2s, by = "i") %>% 
+    dplyr::select(-i) %>%
+    dplyr::mutate(r = 1:n())
 
- test.z <- dplyr::filter(test.x, r %in% test.y$r) %>%
-   dplyr::select(-r) %>%
-   dplyr::mutate(S = S.x + S.y) %>%
-   dplyr::filter(S == min(S))
+  test.y <- test.x %>%
+    dplyr::select(-S.x, -S.y) %>%
+    tidyr::gather(locus, allele, -hap.x, -hap.y, -r) %>%
+    dplyr::arrange(r, hap.x, hap.y, locus) %>%
+    dplyr::group_by(r) %>%
+    dplyr::filter(all(x$allele %in% allele)) %>%
+    dplyr::ungroup()
 
- out.x <- dplyr::select(test.z, dplyr::ends_with(".x")) %>%
-   setNames(sub("\\.x", "", names(.)))
- out.y <- dplyr::select(test.z, dplyr::ends_with(".y")) %>%
-   setNames(sub("\\.y", "", names(.)))
+  test.z <- dplyr::filter(test.x, r %in% test.y$r) %>%
+    dplyr::select(-r) %>%
+    dplyr::mutate(S = S.x + S.y) %>%
+    dplyr::filter(S == min(S))
 
- out <- dplyr::bind_rows(out.x, out.y) %>%
-   dplyr::arrange(hap)
+  out.x <- dplyr::select(test.z, dplyr::ends_with(".x")) %>%
+    setNames(sub("\\.x", "", names(.)))
+  out.y <- dplyr::select(test.z, dplyr::ends_with(".y")) %>%
+    setNames(sub("\\.y", "", names(.)))
 
- out %>%
+  out <- dplyr::bind_rows(out.x, out.y) %>%
+    dplyr::arrange(hap)
+
+  if (nrow(out) == 2L && all(c("a1", "a2") %in% out$hap)) {
+    return(out)
+  }
+
+  min.hap <- dplyr::filter(out, S == min(S))
+
+  hom <- 
+    dplyr::select(out, -hap, -S) %>%
+    sapply(function(a) length(unique(a)) == 1) %>%
+    which() %>%
+    names()
+
+  not_amb <- 
+    dplyr::select(min.hap, -hap, -S) %>%
+    sapply(function(a) length(unique(a)) == 1) %>%
+    which() %>%
+    names()
+
+  not_amb <- not_amb[! not_amb %in% hom]
+
+  df_not_amb <- dplyr::anti_join(out, min.hap, by = "hap")
+
+  for (i in seq_along(not_amb)) {
+    df_not_amb <- dplyr::anti_join(df_not_amb, min.hap, by = not_amb[i])
+  }
+
+  dplyr::bind_rows(min.hap, df_not_amb) %>%
     dplyr::group_by(hap) %>%
     dplyr::summarize_all(. %>% unique %>% paste(collapse = "/")) 
 }
