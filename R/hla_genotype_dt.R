efa3cd5766f360a2002006879bfe3589b95893a4
VitorAguiar <vitor.aguiar@me.com>
2017-09-11 10:50:16 -0300

    use mean instead of unique when summarizing counts in order to avoid non-uniqueness due to rounding

--- R/hla_genotype_dt.R
+++ R/hla_genotype_dt.R
@@ -10,8 +10,8 @@
 hla_genotype_dt <- function(hla_quants, th = 0.05) {
 
   data.table::setDT(hla_quants)
-  hla_quants[, r := data.table::frank(-tpm, ties.method = "dense"), 
-	     by = .(subject, locus)]
+  
+  hla_quants[, r := data.table::frank(-tpm, ties.method = "dense"), by = .(subject, locus)]
 
   top2 <- 
     hla_quants[r == 1 | r == 2
@@ -19,15 +19,15 @@ hla_genotype_dt <- function(hla_quants, th = 0.05) {
 	     ][maxtpm == 0 | (tpm/maxtpm > th)]
   
   out <- 
-    top2[, .(allele = paste(allele, collapse = "="), 
-	     est_counts = unique(est_counts)), 
+    top2[, .(allele = paste(allele, collapse = "="), est_counts = mean(est_counts)), 
          by = .(subject, gene_id, locus, tpm)
        ][, n := .N, by = .(subject, gene_id, locus)
-       ][n == 1, `:=`(est_counts = round(est_counts/2, 1), tpm = tpm/2)
+       ][n == 1, `:=`(est_counts = est_counts/2L, tpm = tpm/2L)
        ][, i := ifelse(n == 1, "1_1", "1")
        ][, .(i = unlist(strsplit(i, "_"))), 
-         by = .(subject, gene_id, locus, allele, tpm, est_counts)]
-  
-  out[, .(subject, gene_id, locus, allele, est_counts, tpm)
-    ][order(subject, locus, allele)]
+         by = .(subject, gene_id, locus, allele, tpm, est_counts)
+       ][, .(subject, gene_id, locus, allele, est_counts, tpm)
+       ][order(subject, locus, allele)]
+
+  out
 }
