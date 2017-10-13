df1a026247676faf17d710087a1d019fe988d789
VitorAguiar <vitor.aguiar@me.com>
2017-10-13 09:29:52 -0300

    rm col_names option and give explicit names

--- R/read_qtltools_rtc.R
+++ R/read_qtltools_rtc.R
@@ -1,15 +1,13 @@
 #' Read in QTLtools rtc output
 #'
 #' @param rtc_output character string. Path to file.
-#' @param col_names. Whether the rtc output has col names. Normal run does, when
-#' a genomic region is selected, it does not.
 #'
 #' @return data.frame
 #'
 #' @export
 
-read_qtltools_rtc <- function(rtc_output, col_names = TRUE)
-  readr::read_delim(rtc_output, col_names = col_names, delim = " ",
+read_qtltools_rtc <- function(rtc_output)
+  readr::read_delim(rtc_output, col_names = FALSE, delim = " ",
 		    col_types = "cccciiiiiiiiiiiiiiiddd") %>%
   purrr::set_names(c("gwas_var", "qtl_var", "gene", "gene_grp", "gwas_chr",
 		     "gwas_pos", "gwas_rank", "qtl_chr", "qtl_pos", "qtl_rank",
