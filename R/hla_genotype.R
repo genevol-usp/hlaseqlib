60fc401fa42d2b609d0a73e289c89e5193484ed7
VitorAguiar <vitor.aguiar@me.com>
2019-03-12 17:18:29 -0300

    take as input preprocessed df instead of file

--- R/hla_genotype.R
+++ R/hla_genotype.R
@@ -9,14 +9,9 @@
 
 hla_genotype <- function(hla_quants, th = 0.05) {
 
-    dat <- readr::read_tsv(hla_quants) %>%
-	dplyr::filter(grepl("^IMGT", Name)) %>%
-	dplyr::mutate(locus = sub("^IMGT_([^\\*]+).+$", "\\1", Name)) %>%
-	dplyr::select(locus, allele = Name, tpm = TPM, counts = NumReads)
+    hla_quants$i <- hla_quants %>% dplyr::group_indices(locus, tpm, counts)
 
-    dat$i <- dat %>% dplyr::group_indices(locus, tpm, counts)
-
-    dat %>%
+    hla_quants %>%
 	dplyr::group_by(locus, i) %>%
 	dplyr::summarise(allele = paste(allele, collapse = "-"),
 		  tpm = sum(tpm),
