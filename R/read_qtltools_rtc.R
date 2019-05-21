3a925e6fc061cab0605025d546ef208c60eff34e
VitorAguiar <vitor.aguiar@me.com>
2019-05-21 10:26:23 -0300

    update

--- R/read_qtltools_rtc.R
+++ R/read_qtltools_rtc.R
@@ -9,9 +9,9 @@
 read_qtltools_rtc <- function(rtc_output)
   readr::read_delim(rtc_output, col_names = FALSE, delim = " ",
 		    col_types = "cccciiiiiiiiiiiiiiiddd") %>%
-  purrr::set_names(c("gwas_var", "qtl_var", "gene", "gene_grp", "gwas_chr",
-		     "gwas_pos", "gwas_rank", "qtl_chr", "qtl_pos", "qtl_rank",
-		     "gene_chr", "gene_pos", "dist_gwas_qtl", "dist_gwas_phen",
-		     "gwas_var_index", "qtl_var_index", "region_start",
-		     "region_end", "n_vars", "rtc", "d_prime", "r_squared"))
+  setNames(c("gwas_var", "qtl_var", "gene", "gene_grp", "gwas_chr",
+	     "gwas_pos", "gwas_rank", "qtl_chr", "qtl_pos", "qtl_rank",
+	     "gene_chr", "gene_pos", "dist_gwas_qtl", "dist_gwas_phen",
+	     "gwas_var_index", "qtl_var_index", "region_start",
+	     "region_end", "n_vars", "rtc", "d_prime", "r_squared"))
 
