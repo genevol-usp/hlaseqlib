577da4ab1624cfe8c222c692639dae8f8f66b40a
VitorAguiar <vitor.aguiar@me.com>
2019-06-26 14:42:23 -0300

    add feature to hla_read_alignment: return separated exons

--- R/hla_read_alignment.R
+++ R/hla_read_alignment.R
@@ -3,6 +3,7 @@
 #' @param locus character string. The HLA locus to be processed (e.g., "A", "DRB1").
 #' @param imgtdb character string. Path to the IMGTHLA directory.
 #' @param exons integer. Range of exons (default is all exons).
+#' @param by_exon logical. Whether to return sequences separated for each exon (default FALSE).
 #'
 #' @return A data.frame.
 #'
@@ -12,11 +13,11 @@
 #' }
 #' \dontrun{
 #' hla_read_alignment(locus = "A", imgtdb = "~/IMGTHLA", exons = c(2, 3))
-#'}
+#' }
 #'
 #' @export
 
-hla_read_alignment <- function(locus, imgtdb, exons = NULL) {
+hla_read_alignment <- function(locus, imgtdb, exons = NULL, by_exon = FALSE) {
  
     locus_nuc <- ifelse(grepl("DRB\\d", locus), "DRB", locus) 
     
@@ -44,7 +45,22 @@ hla_read_alignment <- function(locus, imgtdb, exons = NULL) {
     hla_df <- hla_df %>%
 	dplyr::filter(sub("^([^\\*]+).+$", "\\1", allele) == locus)
 
-    if (!is.null(exons) && is.numeric(exons)) {
+    if (is.null(exons) && !by_exon) {
+
+	hla_df <- hla_df %>% 
+	    dplyr::mutate(cds = gsub("\\|", "", cds))
+    
+    } else if (is.null(exons) && by_exon) {
+
+	hla_df <- hla_df %>%
+	    dplyr::mutate(cds = strsplit(cds, "\\|")) %>%
+	    tidyr::unnest() %>%
+	    dplyr::group_by(allele) %>%
+	    dplyr::mutate(exon = seq_len(dplyr::n())) %>%
+	    dplyr::select(allele, exon, cds) %>% 
+	    dplyr::ungroup()
+    
+    } else if (!is.null(exons) && is.numeric(exons) && !by_exon) {
     
 	hla_df <- hla_df %>%
 	    dplyr::mutate(cds = strsplit(cds, "\\|")) %>%
@@ -55,10 +71,18 @@ hla_read_alignment <- function(locus, imgtdb, exons = NULL) {
 	    dplyr::filter(exon %in% exons) %>%
 	    dplyr::summarise(cds = paste(cds, collapse = "")) %>%
 	    dplyr::ungroup()
-    } 
+    
+    } else if (!is.null(exons) && is.numeric(exons) && by_exon) {
 	
-    hla_df <- hla_df %>% 
-	dplyr::mutate(cds = gsub("\\|", "", cds))
-
+	hla_df <- hla_df %>%
+	    dplyr::mutate(cds = strsplit(cds, "\\|")) %>%
+	    tidyr::unnest() %>%
+	    dplyr::group_by(allele) %>%
+	    dplyr::mutate(exon = seq_len(dplyr::n())) %>%
+	    dplyr::ungroup() %>%
+	    dplyr::select(allele, exon, cds) %>% 
+	    dplyr::filter(exon %in% exons)
+    }
+   
     hla_df
 }
